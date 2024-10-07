08f40fdcd53bb5aa90069926d271227d5ae2e04e
swafwana <swafwanasheri17@gmail.com>
2024-10-07 13:45:22 +0530

    Add files via upload

---
+++ Sep23-27/area.py
@@ -0,0 +1,12 @@
+radius=int(input("Enter radius"))
+area1=3.14*radius**2
+print("Area of circle is",area1)
+base=int(input("Enter base"))
+height=int(input("Enter height"))
+area2=0.5*base*height
+print("Area of triangle is",area2)
+length=int(input("Enter length of rectangle"))
+breadth=int(input("Enter breadth of rectangle"))
+area3=length*breadth
+print("Area of traingle is",area3)
+
---
+++ Sep23-27/arithmetic.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter a number"))
+num2=int(input("Enter another number"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+print("sum is ",sum)
+print("Difference is  ",difference)
+print("Product is",product)
+print("Quotient is",quotient)
