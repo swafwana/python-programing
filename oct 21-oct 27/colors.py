477e85ad938802dad065bb90b7cb629cae7e31a1
swafwana <swafwanasheri17@gmail.com>
2024-10-30 10:38:19 +0530

    Delete oct 21-oct 27 directory

--- oct 21-oct 27/colors.py
+++
@@ -1,3 +0,0 @@
-color=input("Enter different colors separated by comma").split(',')
-print("First color is :",color[0])
-print("Last color is:",color[-1])
--- oct 21-oct 27/dollar.py
+++
@@ -1,3 +0,0 @@
-string=input("Enter a string")
-modified_string=string[0]+string[1:].replace(string[0],'$')
-print(modified_string)
