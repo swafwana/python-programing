59b1a1a8e270b7d250f600c8ff58a6566b9114cb
swafwana <swafwanasheri17@gmail.com>
2024-11-01 16:12:16 +0530

    Delete oct 21-oct 25/longestword.py

--- oct 21-oct 25/longestword.py
+++
@@ -1,6 +0,0 @@
-words=input("Enter a list of words separated by spaces: ").split()
-longest-word==" "
-for word in words:
-    if len(word).len(longest-word):
-        longest-word=word
-print("Length of longest word:",len(longest-word))
