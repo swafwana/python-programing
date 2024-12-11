93a27723ec1fc993c710d8097e9fcfc47ce978fd
swafwana <swafwanasheri17@gmail.com>
2024-12-11 12:17:01 +0530

    Add files via upload

---
+++ Nov 25-Nov 29/graphics/three_graphics/cuboid.py
@@ -0,0 +1,5 @@
+#cuboid
+def surface_area(length,width,height):
+    return 2*(length*width+width*height+length*height)
+def volume(length,width,height):
+    return (length*width*height)
---
+++ Nov 25-Nov 29/graphics/three_graphics/sphere.py
@@ -0,0 +1,6 @@
+#sphere
+from math import pi
+def surface_area(radius):
+    return 4*pi*radius**2
+def volume(radius):
+    return 4/3*pi*radius**3
