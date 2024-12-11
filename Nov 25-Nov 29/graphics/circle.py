c5bcfabebbd10bf272bbbb1e672e9fa7c1643ae2
swafwana <swafwanasheri17@gmail.com>
2024-12-11 12:16:29 +0530

    Add files via upload

---
+++ Nov 25-Nov 29/graphics/circle.py
@@ -0,0 +1,6 @@
+#circle
+from math import pi
+def area(radius):
+    return pi*radius**2
+def perimeter(radius):
+    return 2*pi*radius
---
+++ Nov 25-Nov 29/graphics/rectangle.py
@@ -0,0 +1,5 @@
+#rectangle
+def area(length,width):
+    return length*width
+def perimeter(length,width):
+    return 2*(length+width)
